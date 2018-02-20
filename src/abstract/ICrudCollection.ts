3441c1f2c9db54b0c3af73a740dd13be6d1dda22
matan <p7586381@pituah.iaf>
2018-02-20 13:06:48 +0200

    code/abstract: create `ICrudCollection` interface

--- src/abstract/ICrudCollection.ts
+++ src/abstract/ICrudCollection.ts
@@ -1,10 +1,8 @@
 import { IDescriptor } from "./IDescriptor";
 
 export interface ICrudCollection {
-    get(): Promise<Array<IDescriptor>>;
-
     /**
-     * 
+     * Create a single item
      * 
      * @param {*} item 
      * @returns {Promise<string>} The id of the created item
@@ -13,7 +11,15 @@ export interface ICrudCollection {
     create(item: any): Promise<string>;
 
     /**
+     * Returns all the items who match the specified `filter` and limits the number of returned item to `limit`.
      * 
+     * @returns {Promise<Array<IDescriptor>>} 
+     * @memberof ICrudCollection
+     */
+    read(filter: any, limit: number): Promise<Array<IDescriptor>>;
+
+    /**
+     * Returns the item that is represented by the specified `id`.
      * 
      * @param {string} id 
      * @returns {Promise<IDescriptor>} 
@@ -22,7 +28,7 @@ export interface ICrudCollection {
     readById(id: string): Promise<IDescriptor>;
 
     /**
-     * 
+     * Updates the item represeted by the specified `id`.
      * 
      * @param {string} id 
      * @param {*} item 
@@ -32,11 +38,18 @@ export interface ICrudCollection {
     update(id: string, item: any): Promise<IDescriptor>;      
 
     /**
+     * Deletes the item represeted by the specified `id`
      * 
-     * 
-     * @param {string} id 
-     * @returns {Promise<IDescriptor>} The deleted item
+     * @param {string} id the id of the desired item to be delted
+     * @returns {Promise<IDescriptor>} `1` if the item was deleted, otherwise `0`
      * @memberof ICrudCollection
      */
-    delete(id: string): Promise<IDescriptor>;
+    deleteById(id: string): Promise<number>;
+
+    /**
+     * Deletes all the items who the match the specified `filter` and the limits the deletion to `limit` items.
+     * 
+     * @memberof ICrudCollection
+     */
+    delete(filter: any, limit: number): Promise<number>;
 }
