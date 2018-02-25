344e59f03f9d60324585e8d831fdb430acf68aa5
matan <p7586381@pituah.iaf>
2018-02-25 17:33:58 +0200

    code/index: add `Cruder`, `IFilterParam`, `ICrudItem` and `FilterOperators`

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { ICrudCollection } from './abstract/ICrudCollection';
+export { IDescriptor } from './abstract/IDescriptor';
+export { FilterOperators } from './abstract/FilterOperators';
+export { ICrudItem } from './abstract/ICrudItem';
+export { IFilterParam } from './abstract/IFilterParam';
+export { Cruder } from './Cruder';
