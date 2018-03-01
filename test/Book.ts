a2de9bf8e25b7fa0745b576b33ecd83942df1426
matan <p7586381@pituah.iaf>
2018-03-01 15:47:40 +0200

    test/user: add books collection

---
+++ test/Book.ts
@@ -0,0 +1,14 @@
+import { ICrudCollection } from "../src/abstract/ICrudCollection";
+import { Page } from "./Page";
+import { CrudItem } from "./CrudItem";
+
+export class Book extends CrudItem{
+    public name: string;
+    public pages: Page[];
+
+    constructor(name: string){
+        super();
+        this.name = name;
+        this.pages = new Array<Page>();
+    }
+}
--- test/User.ts
+++ test/User.ts
@@ -1,11 +1,29 @@
-import { IDescriptor } from "../src/index";
+import { IDescriptor, IParam } from "../src/index";
 import { CrudItem } from "./CrudItem";
+import { ICrudCollection } from "../src/abstract/ICrudCollection";
+import { Book } from "./Book";
+import { ItemsManager } from "./ItemsManager";
 
-export class User extends CrudItem{    
+export class User extends CrudItem{
+    private _booksCounter = 0;
+    private _books: ItemsManager<Book>;
+
     constructor(public name: string, public height: number){
         super();
-
+        
         this.name = name;
         this.height = height;
+
+        this._books = new ItemsManager<Book>();
+    }
+
+    public describe(): any{
+        let description = {
+            name: this.name,
+            height: this.height,
+            books: this._books.describe(),
+        }
+
+        return description;
     }
 }
