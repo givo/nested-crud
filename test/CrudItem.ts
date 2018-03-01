d4bb3e93893e1dd5a32d63d9d83227b8ef42c1d8
matan <p7586381@pituah.iaf>
2018-03-01 15:11:31 +0200

    test/crudItem: not updating non primitive types

--- test/CrudItem.ts
+++ test/CrudItem.ts
@@ -3,7 +3,7 @@ import { IDescriptor } from "../src/abstract/IDescriptor";
 export class CrudItem implements IDescriptor{    
     public update(fields: any){
         for(let param in fields){
-            if((<any>this)[param]){
+            if((<any>this)[param] && (<any>fields)[param] !== Object((<any>fields)[param])){
                 (<any>this)[param] = fields[param];
             }
         }
