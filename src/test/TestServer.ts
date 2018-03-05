c5d49b158b2ad5acefc65c549462d94567456651
matan <p7586381@pituah.iaf>
2018-03-05 16:17:19 +0200

    test/server: create simple test server

--- src/test/TestServer.ts
+++ src/test/TestServer.ts
@@ -7,17 +7,31 @@ import { Cruder } from '../Cruder';
 import { ItemsManager } from './abstract/ItemsManager';
 import { User } from './foundations/User';
 import { Book } from './foundations/Book';
+import { Page } from './foundations/Page';
 
 let app = express();
 let cruder = new Cruder();
 
 let usersManager = new ItemsManager<User>(<(new () => User)>User);
 
-// create beni
+// create Beni
 let beni = new User("Beni", 165);
+
+// create Beni's books
 let beniBooks = beni.getCollection("books");
 beniBooks.create(new Book("Harry Potter"));
 beniBooks.create(new Book("Lord of the Rings"));
+let book = new Book("Rich Father Poor Father");
+beniBooks.create(book);
+
+// create book's pages
+let pages = book.getCollection("pages");
+pages.create(new Page(0, "Avishai is the king"));
+pages.create(new Page(1, "Avishai is the prince"));
+pages.create(new Page(2, "Avishai is the queen"));
+pages.create(new Page(3, "Avishai is the knight"));
+pages.create(new Page(4, "Avishai is the slave"));
+pages.create(new Page(5, "Avishai is the goblin"));
 
 // create all users
 usersManager.create(new User("Yosi", 174));
@@ -27,8 +41,10 @@ usersManager.create(new User("Shimon", 180));
 
 let usersREST = cruder.listen('/users/:userId', usersManager);
 let booksREST = cruder.listen('/users/:userId/books/:bookId', usersManager);
+let pagesREST = cruder.listen('/users/:userId/books/:bookId/pages/:pageId', usersManager);
 app.use(usersREST);
 app.use(booksREST);
+app.use(pagesREST);
 
 app.listen(3000, () => {
     console.log('listening on port 3000');
