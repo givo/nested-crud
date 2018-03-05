2c8ccbdb00c984033961546f3f2a767d493bb18c
matan <p7586381@pituah.iaf>
2018-03-05 16:14:44 +0200

    test/book: return description of pages

--- src/test/foundations/Book.ts
+++ src/test/foundations/Book.ts
@@ -1,22 +1,23 @@
 import { ICrudCollection } from "../../abstract/ICrudCollection";
 import { Page } from "./Page";
 import { CrudItem } from "../abstract/CrudItem";
+import { ItemsManager } from "../abstract/ItemsManager";
 
 export class Book extends CrudItem{
     public name: string;
-    public pages: Page[];
+    public pages: ItemsManager<Page>;
 
     constructor(name: string = " "){
         super();
         this.name = name;
-        this.pages = new Array<Page>();
+        this.pages = new ItemsManager<Page>(<(new () => Page)>Page);
     }
 
     public describe(): any{
         let description = {
             id: this.id,
             name: this.name,            
-            pages: this.pages,
+            pages: this.pages.describe(),
         }
 
         return description;
